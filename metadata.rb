63f7bc5627303ff0512df4873f32556a1537a27f
Lars Kleen <l.kleen@bigpoint.net>
2013-02-26 16:32:48 +0100

    deleted useless folder

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,14 @@ license          'All rights reserved'
 description      'Installs/Configures setup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe "database", "Empty placeholder"
+recipe "database::master", "Creates application specific user and database"
+
+depends "mysql", ">= 1.3.0"
+
+%w{ debian ubuntu centos suse fedora redhat scientific amazon }.each do |os|
+  supports os
+end
+
+
