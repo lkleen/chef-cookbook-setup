85b7b1a0acbc497d94cca1208b1af80aa24752b1
Lars Kleen <l.kleen@bigpoint.net>
2013-02-26 16:41:31 +0100

    updated version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,13 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures setup'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
+
+recipe "database", "Empty placeholder"
+recipe "database::master", "Creates application specific user and database"
+
+depends "mysql", ">= 1.3.0"
+
+%w{ debian ubuntu centos suse fedora redhat scientific amazon }.each do |os|
+  supports os
+end
