58a33720a8516de7bbcd7641d5a878e87ddf57ae
Lars Kleen <l.kleen@bigpoint.net>
2013-02-27 11:39:48 +0100

    added default attributes

--- attributes/database.rb
+++ attributes/database.rb
@@ -1,4 +1,6 @@
-include_attribute "mysql"
+default['mysql']['server_debian_password'] = "default"
+default['mysql']['server_root_password'] = "default"
+default['mysql']['server_repl_password'] = "default"
 
 default['setup']['database']['name'] = "db01"
 default['setup']['database']['user'] = "vagrant"
