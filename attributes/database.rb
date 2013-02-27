a9f942f7d39ee43fa6bcaadef37c4853070990d8
Lars Kleen <l.kleen@bigpoint.net>
2013-02-27 13:15:57 +0100

    fixed typo in recipe

--- attributes/database.rb
+++ attributes/database.rb
@@ -1,4 +1,1 @@
-default['setup']['database']['name'] = "db01"
-default['setup']['database']['user'] = "vagrant"
-default['setup']['database']['pass'] = "vagrant"
 
--- recipes/database.rb
+++ recipes/database.rb
@@ -3,8 +3,7 @@ include_recipe "database::mysql"
 mysql_connection_info = {
     :host => "localhost",
     :username => 'root',
-    #:password => node['mysql']['server_root_password']
-    :password => "whyyounothere"
+    :password => node['mysql']['server_root_password']
 }
 
 mysql_database node['setup']['database']['name'] do
@@ -14,7 +13,7 @@ end
 
 mysql_database_user node['setup']['database']['user'] do
   connection mysql_connection_info
-  password node['setup']['database']['password']
+  password node['setup']['database']['pass']
   database_name node['setup']['database']['name']
   host '%'
   action :grant
