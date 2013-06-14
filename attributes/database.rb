1668d24a4c5fde6113c7d9d3c209a694e02a5f1d
Lars Kleen <l.kleen@bigpoint.net>
2013-06-14 17:11:56 +0200

    implemented setup for multiple databases

--- attributes/database.rb
+++ attributes/database.rb
@@ -1,1 +1,19 @@
+# to add datebases here add json objects of the following type in an array
+databases = [
+    {
+        :host => 'host1',
+        :user => 'user1',
+        :pass => 'user1',
+        :name => 'database1',
+    },
+    {
+        :host => 'host2',
+        :user => 'user2',
+        :pass => 'user2',
+        :name => 'database2',
+    }
+]
+
+default['setup']['databases'] = []
+
 
